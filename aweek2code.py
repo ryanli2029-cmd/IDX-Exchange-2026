f4621f8ae3c05e8adae6ea87d2d25a88af050da6
ryanli2029-cmd <ryanli2029@u.northwestern.edu>
2026-04-13 19:01:33 -0500

    fixed a logic error in figuring out what columns have >90% null values

--- aweek2code.py
+++ aweek2code.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 sold = pd.read_csv('Combined_CRMLSSold_Residential.csv', low_memory=False)
 listings = pd.read_csv('Combined_CRMLSListing_Residential.csv', low_memory=False)
@@ -11,16 +12,23 @@ listings = listings[listings['PropertyType'] == 'Residential']
 print("\n Unique Property Types found: ")
 print(sold['PropertyType'].unique())
 
+#sold = sold.replace(r'^\s*$', np.nan, regex=True)
+sold = sold.replace(['None', 'null', 'NULL', 'NaN'], np.nan)
+
 #a series of the null counts for each column
 null_counts = sold.isnull().sum()
 
 #a series containing the null percentages for each column
-null_percents = (null_counts / len(sold))
+null_percents = (null_counts / len(sold)) * 100
+
+print("\nNull percents per column")
+print(null_percents)
 
 #filter the series to only contain 90% or higher
 high_null_percents = null_percents[null_percents > 90]
 print("\nColumns with >90% missing values:")
 print(high_null_percents)
+
 
 #numeric distribtion summary
 print("\nNumeric Distribution Summary: ")
