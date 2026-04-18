d79509881ff089c94af1a7e969b6f30c271b8f2f
ryanli2029-cmd <ryanli2029@u.northwestern.edu>
2026-04-18 16:27:51 -0500

    Completed both parts of deliverable

--- aweek2code.py
+++ aweek2code.py
@@ -35,3 +35,39 @@ print("\nNumeric Distribution Summary: ")
 numeric_cols = ['ClosePrice', 'LivingArea', 'DaysOnMarket']
 filtered = sold[numeric_cols]
 print(filtered.describe())
+
+#part 2 of deliverable (enrichment)
+url = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=MORTGAGE30US"
+mortgage = pd.read_csv(url, parse_dates=['observation_date'])
+mortgage.columns = ['date', 'rate_30yr_fixed']
+mortgage['year_month'] = mortgage['date'].dt.to_period('M')
+mortgage_monthly=(mortgage.groupby('year_month')['rate_30yr_fixed'].mean().reset_index())
+
+sold['year_month'] = pd.to_datetime(sold['CloseDate']).dt.to_period('M')
+listings['year_month'] = pd.to_datetime(listings['ListingContractDate']).dt.to_period('M')
+
+#merge
+sold_with_rates = sold.merge(mortgage_monthly, on='year_month', how='left')
+listings_with_rates = listings.merge(mortgage_monthly, on='year_month', how='left')
+
+print("\nNull mortgage rates in Sold dataset:", sold_with_rates['rate_30yr_fixed'].isnull().sum())
+print("\nNull mortgage rates in Listings dataset:", listings_with_rates['rate_30yr_fixed'].isnull().sum())
+sold_with_rates.to_csv('Combined_CRMLSSold_Residential_Enriched.csv', index=False)
+listings_with_rates.to_csv('Combined_CRMLSListing_Residential_Enriched.csv', index=False)
+print("\Sold and Listings Enriched CSV Files Saved Successfully")
+
+print("\nSold Dataset Preview")
+# Previewing the sold data using CloseDate
+print(
+    sold_with_rates[
+        ['CloseDate', 'year_month', 'ClosePrice', 'rate_30yr_fixed']
+    ].head()
+)
+
+print("\nListings Dataset Preview")
+# Previewing the listings data using ListingContractDate
+print(
+    listings_with_rates[
+        ['ListingContractDate', 'year_month', 'ListPrice', 'rate_30yr_fixed']
+    ].head()
+)
